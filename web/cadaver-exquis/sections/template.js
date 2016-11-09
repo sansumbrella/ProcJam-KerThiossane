f306eb7d9c29b44fd7db9ae3fae46943168fc1a8
David Wicks <wicks.db@gmail.com>
2016-11-09 21:49:50 +0000

    French guidance in template.js
    With much help from Rebecca.

--- web/cadaver-exquis/sections/template.js
+++ web/cadaver-exquis/sections/template.js
@@ -1,8 +1,7 @@
-// The name of this variable must match the name of your file to work.
-// e.g. if your file is "wobble.js", the variable should be named "wobble".
+// Le nom de cette variable ci est la même de ce fichier.
+// e.g. pour un fichier "wobble.js", cette variable est appelé "wobble".
 var template = (function() {
-    // Define a function that creates your section.
-    // This function will return an object that has an "update" and "draw" function.
+    // Cet function crée un objet javascript avec functions "update" et "draw".
     function create (frame) {
         // Set up some parameters for your drawing.
         var x = util.random(frame.gauche, frame.droit);
@@ -11,31 +10,40 @@ var template = (function() {
         var forme = new Path2D();
         forme.rect(-50, -50, 100, 100);
 
-        // use this function to change your drawing's variables to respond to the previous drawing
+        // Cet function va etre appelé dans tous les frame de l'animation.
+        // Utilisez pour changer vos variables.
         function update(input, temps, dt) {
             angle = temps;
             return input;
         }
 
-        // use this function to draw your section to screen
-        // you can draw in the area [[0, 0], [width, height]]
+        // Cet function va etre appelé dans tous les frame de l'animation.
+        // Utilisez pour dessiner vos formes.
         function draw(ctx) {
+            // Effacer le dessin dernier.
             ctx.clearRect(frame.gauche, frame.haut, frame.largeur, frame.hauteur);
 
+            // Dessiner "fills" avec le couleur rgba(rouge, vert, bleu, transparent)
             ctx.fillStyle = util.rgba(255, 255, 0, 1.0);
 
+            // Dessiner une form a la position (x, y)
             ctx.save();
             ctx.translate(x, y);
             ctx.rotate(angle);
             ctx.fill(forme);
             ctx.restore();
 
+            // Dessiner "strokes" avec le couleur hsva(teinte, saturation, éclat, transparent)
+            ctx.strokeStyle = util.hsva(0.5, 1.0, 1.0, 1.0);
+            // Dessiner une forme a la position (mouseX, mouseY)
             ctx.save();
             ctx.translate(util.mouseX, util.mouseY);
-            ctx.fill(forme);
+            ctx.stroke(forme);
             ctx.restore();
         }
 
+        // Ici, nous créons un objet avec functions "update" et "draw", qui utilise
+        // ces functions que nous avons défini au-dessus.
         return {
             title: "Template",
             author: "David Wicks",
@@ -44,8 +52,8 @@ var template = (function() {
         };
     }
 
-    // This is the value of the top-level "line" variable.
-    // it should have one member named "create" that creates your object.
+    // Ici, nous créons un object avec function "create", qui crée notre objet
+    // que nous avons défini au-dessus.
     return {
         create: create
     };
