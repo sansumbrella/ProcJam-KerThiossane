c5cdff2e8137e852a91183856befe34dc5af6184
David Wicks <wicks.db@gmail.com>
2016-11-13 10:36:59 +0000

    Randomize number of columns.

--- web/cadaver-exquis/settings.js
+++ web/cadaver-exquis/settings.js
@@ -1,4 +1,4 @@
 var settings = {
 	sections: ["live", "circle", "line", "image", "demba", "lamb_ji", "demba1", "pi", "diallo", "exemple", "ACM"],
-	columns: 5
+	columns: Math.floor(3 + (Math.random() * 12))
 }
